1b1a9e28ac38b8cb0ef8aafce121fe3165f13cf8
KainH13 <kneuholdhuber@gmail.com>
2021-09-14 12:47:27 -0700

    completed button clicker JS

---
+++ Assignments/Button_Clicker/buttonClicker.js
@@ -0,0 +1,23 @@
+// TODO Change text on login to logout when clicked
+
+function changeText(element) {
+    if (element.innerText == "login") {
+        element.innerText = "logout"
+    }
+    else {
+        element.innerText = "login"
+    }
+}
+
+// TODO Remove add definition button when it is clicked
+
+function remove(element) {
+    element.remove()
+}
+
+// TODO Alert "Ninja was liked" when clicking a like button
+
+function likeAlert() {
+    alert("Ninja was liked");
+    // console.log("like button clicked")
+}
