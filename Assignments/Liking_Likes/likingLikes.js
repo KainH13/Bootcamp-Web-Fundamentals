52f1fa4614deb69752ac3a83ebb3a2a440f10215
KainH13 <kneuholdhuber@gmail.com>
2021-09-15 18:31:10 -0700

    completing js for liking likes

--- Assignments/Liking_Likes/likingLikes.js
+++ Assignments/Liking_Likes/likingLikes.js
@@ -1,8 +1,6 @@
-
-
 function increaseLikes(likes) {
-    var likes = document.querySelector("#likes1").innerText
-    likes++
-    document.querySelector("#likes1").innerText = likes
-    console.log(`Added 1 like for a total of ${likes}`)
+    var likeValue = document.querySelector(likes).innerText
+    likeValue++
+    document.querySelector(likes).innerText = likeValue
+    console.log(`Added 1 like to ${likes} for a total of ${likeValue}`)
 }
